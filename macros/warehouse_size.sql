ed714b0296c1daf7c9d6ed249528c577f56fd2eb
Martin Guindon <14781590+MartinGuindon@users.noreply.github.com>
2020-04-27 09:42:04 -0400

    Fix comment formatting on Github

--- macros/warehouse_size.sql
+++ macros/warehouse_size.sql
@@ -8,34 +8,34 @@
         {% set full_wh = var('snowflake_utils:full_refresh_run_warehouse', none) %}
         {% set inc_wh = var('snowflake_utils:incremental_run_warehouse', none) %}
 
-        {#- use alternative warehouse if initial run #}
+        {#-- use alternative warehouse if initial run #}
         {% if relation is none and initial_wh is not none %}
             {{ dbt_utils.log_info("Initial Run - Using alternative warehouse " ~ initial_wh | upper) }}
             {% do return(initial_wh) %}
 
-        {#- use alternative warehouse if full-refresh run #}
+        {#-- use alternative warehouse if full-refresh run #}
         {% elif flags.FULL_REFRESH and full_wh is not none %}
             {{ dbt_utils.log_info("Full Refresh Run - Using alternative warehouse " ~ full_wh | upper) }}
             {% do return(full_wh) %}
 
-        {#- use alternative warehouse if incremental run #}
+        {#-- use alternative warehouse if incremental run #}
         {% elif relation is not none and inc_wh is not none %}
             {{ dbt_utils.log_info("Incremental Run - Using alternative warehouse " ~ inc_wh | upper) }}
             {% do return(inc_wh) %}        
 
-        {#- use target warehouse if variable not configured for a condition #}
+        {#-- use target warehouse if variable not configured for a condition #}
         {% else %}
             {{ dbt_utils.log_info("Using target warehouse " ~ target.warehouse | upper) }}
             {% do return(target.warehouse) %}
 
         {% endif %}
 
-    {#- use target warehouse if model is not incremental #}
+    {#-- use target warehouse if model is not incremental #}
     {% elif execute and model.config.materialized != 'incremental' %}
         {{ dbt_utils.log_info("Using target warehouse " ~ target.warehouse | upper) }}
         {% do return(target.warehouse) %}
 
-    {#- use target warehouse for parsing #}
+    {#-- use target warehouse for parsing #}
     {% else %}
         {% do return(target.warehouse) %}
 
