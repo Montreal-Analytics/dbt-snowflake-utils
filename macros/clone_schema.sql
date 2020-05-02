e63ed75d973fb07c8e3c2262fa69183b5f0fa4d6
Jonathan Talmi <jonathan@snaptravel.com>
2020-05-01 21:34:17 -0400

    add clone schema macro

---
+++ macros/clone_schema.sql
@@ -0,0 +1,15 @@
+{% macro clone_schema(source_schema, destination_schema) %}
+  
+  {% if source_schema is not none and destination_schema is not none and source_schema != destination_schema %}
+    
+    {{ log("Cloning existing schema " ~ source_schema ~ " into schema " ~ destination_schema, info=True) }}
+    
+    {% call statement('clone_schema', fetch_result=True, auto_begin=False) -%}
+        CREATE OR REPLACE SCHEMA {{ destination_schema }} CLONE {{ source_schema }}
+    {%- endcall %}
+    
+    {%- set result = load_result('clone_schema') -%}
+    {{ log(result['data'][0][0], info=True)}}
+
+  {% endif %}
+{% endmacro %}
