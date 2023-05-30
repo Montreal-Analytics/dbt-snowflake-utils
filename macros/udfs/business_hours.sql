3253a67ebc6c638f9186dca1cb8753b629f90d5c
Cris T Seaton <seaton.cris@gmail.com>
2023-05-30 11:05:47 -0400

    change early return and accomodate negative calculations with max()

--- macros/udfs/business_hours.sql
+++ macros/udfs/business_hours.sql
@@ -17,14 +17,17 @@ import numpy as np
 def business_hours_py(start_datetime, end_datetime, country='US'):
 
     # Year range for holidays spanning the years below
+    
     years = [*range(1990,2030)]
     
     # Hard coded business hours, opening and closing times
+
     workhours_per_day = 8
     opening_hour = 9
     closing_hour = 17
 
     # Create open and closing datetimes to establish day 0 and day n durations
+
     first_day_closed = start_datetime.replace(hour=closing_hour, minute=0, second=0)
     last_day_opened = end_datetime.replace(hour=opening_hour, minute=0, second=0)
 
@@ -34,27 +37,34 @@ def business_hours_py(start_datetime, end_datetime, country='US'):
         end_datetime.date(),
         holidays = holiday_list)
     
-    # Calculate hours for full business days (excluding first day and last day)
-    complete_days_hours = (days - 2) * workhours_per_day
+    # Calculate hours for full business days (excluding first day, last day is automatically excluded in busday_count)
+    
+    complete_days_hours = (days - 1) * workhours_per_day
 
-    # Jump forward to the next non-holiday weekday
+    # Jump forward to the next non-holiday
+    # When start_datetime is iterated beyond end_datetime, this indicates entirety of hours occuring during holidays or
+    # weekend, therefore return 0 via max(0,business_hours). 
+
     while start_datetime.date() in holiday_list or start_datetime.weekday() in [5,6]: 
-        start_datetime = start_datetime.replace(
-            hour = opening_hour, minute = 0, second = 0
-            ) + timedelta(days = 1)
+        start_datetime = start_datetime + timedelta(days=1)
 
-    # Jump back to the last non-holiday weekday
-    while end_datetime.date() in holiday_list or end_datetime.weekday() in [5,6]: 
-        end_datetime = end_datetime.replace(
-            hour = closing_hour, minute = 0, second = 0
-            ) - timedelta(days = 1)
-        
     if start_datetime.date() == end_datetime.date():
-        return round((end_datetime - start_datetime).seconds/60/60,2)
+        business_hours = round((end_datetime - start_datetime).seconds/60/60,2)
+        return max(0, business_hours)
     else:
         duration_day_zero = round((first_day_closed - start_datetime ).seconds/60/60, 2)
-        duration_day_n = round((end_datetime - last_day_opened).seconds/60/60, 2)
-        return round(duration_day_zero + duration_day_n + complete_days_hours, 2)
+
+        if end_datetime.date() in holiday_list or end_datetime.weekday() in [5,6]: 
+            # If end_datetime falls on a holiday or weekend, the previous full day is accounted for in 
+            # complete_day_hours calc so early return just duration_day_zero and complete_day_hours
+            business_hours = round(duration_day_zero + complete_days_hours, 2)
+            return max(0,business_hours)
+        else:
+            # If end_datetime does not fall on holiday or weekend, calculate last day's working hours 
+            # and return total sum
+            duration_day_n = (end_datetime - last_day_opened).seconds/60/60
+            business_hours = round(duration_day_zero + complete_days_hours + duration_day_n, 2)
+            return max(0, business_hours)
 
 $$
 
